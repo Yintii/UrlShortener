124a658ff0480c59dc8f0766a147f6b7cd76072b
yintii <keleheart1@gmail.com>
2023-08-24 20:58:57 -0700

    corrected my correction which was wrong

--- app/controllers/short_links_controller.rb
+++ app/controllers/short_links_controller.rb
@@ -3,7 +3,7 @@ class ShortLinksController < ApplicationController
   def redirect
     short_link = ShortLink.find_by(short_link: params[:short_link])
     if short_link
-      redirect_to short_link.original_url, status: 301
+      redirect_to short_link.original_link, status: 301
     else
       redirect_to root_path, alert: "Short link not found", status: 404
     end
