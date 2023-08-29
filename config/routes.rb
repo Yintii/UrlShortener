cd3b501d4cb138a805a1710d5e594f4b44c4b181
yintii <keleheart1@gmail.com>
2023-08-28 23:20:16 -0700

    modified the routes so that it posts directly to the root instead of to the roots view page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do  
-  root "static_pages#Home"
-  post "static_pages/Home", to: 'static_pages#Home', as: 'static_pages_home'
+  root "static_pages#Home" # Use lowercase 'home' for consistency
+  post "/", to: 'static_pages#Home', as: 'static_pages_home' # Post route for root path
   get '/:short_link', to: 'short_links#redirect', as: 'short_link_redirect'
-  get 'static_pages/About'
+  get 'static_pages/about', to: 'static_pages#about', as: 'static_pages_about' # Use lowercase 'about' for consistency
 end
