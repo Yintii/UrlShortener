b6a0d083cb36e21f58e3dcfff1ddfe9271df2cbf
Kele Heart <hackerman@MacBookPro.lan>
2026-01-02 19:01:43 -0800

    clobbered and precompiled prod assets

--- public/assets/controllers/index-7e7509ec745782e59784a9704c22c1b305798435572539c02723202403c1041f.js
+++
@@ -1,5 +0,0 @@
-// app/javascript/controllers/index.js
-import { application } from "./application"
-import NavbarController from "./navbar_controller"
-
-application.register("navbar", NavbarController);
---
+++ public/assets/controllers/index-cb4b9355cf52e62065318eeca3842b499477293ad4fd2231af66ef2c499a2a2c.js
@@ -0,0 +1,5 @@
+// app/javascript/controllers/index.js
+import { application } from "./application.js"
+import NavbarController from "./navbar_controller.js"
+
+application.register("navbar", NavbarController);
