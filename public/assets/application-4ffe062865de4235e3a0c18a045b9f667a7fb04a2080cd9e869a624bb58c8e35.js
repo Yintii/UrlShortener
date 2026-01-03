e565a2bf64332085dc0f2fb6fec3f169a45305da
Kele Heart <hackerman@MacBookPro.lan>
2026-01-02 19:24:37 -0800

    idk just work plz

--- public/assets/application-4ffe062865de4235e3a0c18a045b9f667a7fb04a2080cd9e869a624bb58c8e35.js
+++ public/assets/application-4ffe062865de4235e3a0c18a045b9f667a7fb04a2080cd9e869a624bb58c8e35.js
@@ -1,6 +1,6 @@
 // app/javascript/application.js
 import { Application } from "@hotwired/stimulus"
-import NavbarController from "./controllers/navbar_controller.js"
+import NavbarController from "controllers/navbar_controller"
 
 const application = Application.start()
 application.register("navbar", NavbarController);
