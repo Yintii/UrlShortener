d101f08c40146997342839fa8938a0f732f3e3c5
Kele Heart <hackerman@MacBookPro.lan>
2026-01-02 18:47:32 -0800

    cowabunga baby

---
+++ public/assets/application-a62777e822d3f4377bdb94c5b5cf915a58bc4c512fc8c73a1b9ea2c982884558.js
@@ -0,0 +1,6 @@
+// app/javascript/application.js
+import { Application } from "@hotwired/stimulus"
+import NavbarController from "./controllers/navbar_controller.js"
+
+const application = Application.start()
+application.register("navbar", NavbarController);
--- public/assets/controllers/index-7e7509ec745782e59784a9704c22c1b305798435572539c02723202403c1041f.js
+++ public/assets/controllers/index-7e7509ec745782e59784a9704c22c1b305798435572539c02723202403c1041f.js
@@ -2,4 +2,4 @@
 import { application } from "./application"
 import NavbarController from "./navbar_controller"
 
-application.register("navbar", NavbarController)
+application.register("navbar", NavbarController);
