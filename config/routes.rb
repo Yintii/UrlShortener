ada2435625bf049476c87363f1e8874695bb592a
Kele Heart <hackerman@Keles-MacBook-Pro.attlocal.net>
2026-01-08 18:31:23 -0800

    About and terms added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do  
   devise_for :users
   root "short_links#home"
+  get '/about', to: 'static_pages#about', as: 'about'
+  get '/terms', to: 'static_pages#terms', as: 'terms_of_service'
   post "/", to: 'short_links#home', as: 'short_link_home' 
   get '/:short_link', to: 'short_links#home', as: 'short_link_redirect'
 end
