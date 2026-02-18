45f9c1713bd77db2a4ac1f4576a8701382be32aa
yintii <keleheart1@gmail.com>
2026-02-18 20:36:44 +0000

    set up user account email confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_08_000000) do
+ActiveRecord::Schema[7.2].define(version: 2026_02_18_200546) do
   create_table "short_links", force: :cascade do |t|
     t.string "original_link"
     t.string "short_link"
@@ -28,6 +28,11 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_08_000000) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
