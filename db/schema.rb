00e652bab57fe937bf200619e85ca4da94cc2eed
yintii <keleheart1@gmail.com>
2026-02-19 19:26:02 -0800

    major redesign

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,20 @@ ActiveRecord::Schema[7.2].define(version: 2026_02_18_200546) do
     t.index ["user_id"], name: "index_short_links_on_user_id"
   end
 
+  create_table "shorten_links", force: :cascade do |t|
+    t.string "givenLink"
+    t.string "shortenedLink"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "shortened_links", force: :cascade do |t|
+    t.string "givenLink"
+    t.string "shortenedLink"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
