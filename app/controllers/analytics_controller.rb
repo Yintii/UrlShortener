fe672a4b662f2cd07f934e2d455d663163be7ee1
yintii <keleheart1@gmail.com>
2026-02-26 22:05:08 +0000

    map geolocation for clickers is complete

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,30 +1,20 @@
 class AnalyticsController < ApplicationController
-    before_action :authenticate_user!
-    before_action :set_short_link
-    
-    def show
-        @click_count = @short_link.click_count
-        @unique_ips = @short_link.link_clicks.unique_ips
-        @clicks_by_day = @short_link.link_clicks.by_day
-    end
-    
-    def analytics
-      @short_link = ShortLink.find(params[:id])
-      @click_count = @short_link.link_clicks.count
-      @unique_ips = @short_link.link_clicks.distinct.count(:ip_address)
-      @clicks_by_day = @short_link.link_clicks.group_by_day(:clicked_at).count
-      
-      # For the map - only clicks that were successfully geocoded
-      @click_locations = @short_link.link_clicks
-                              .where.not(latitude: nil)
-                              .select(:latitude, :longitude, :city, :country)
-                              .map { |c| { lat: c.latitude, lng: c.longitude, city: c.city, country: c.country } } || []
-    end
-    
-    
-    private
-    
-    def set_short_link
-        @short_link = current_user.short_links.find(params[:short_link_id])
-    end
+  before_action :authenticate_user!
+  before_action :set_short_link
+
+  def show
+    @click_count = @short_link.click_count
+    @unique_ips = @short_link.link_clicks.unique_ips
+    @clicks_by_day = @short_link.link_clicks.by_day
+    @click_locations = @short_link.link_clicks
+                                  .where.not(latitude: nil)
+                                  .select(:latitude, :longitude, :city, :country)
+                                  .map { |c| { lat: c.latitude, lng: c.longitude, city: c.city, country: c.country } }
+  end
+
+  private
+
+  def set_short_link
+    @short_link = current_user.short_links.find(params[:short_link_id])
+  end
 end
--- app/models/link_click.rb
+++ app/models/link_click.rb
@@ -12,13 +12,16 @@ class LinkClick < ApplicationRecord
   def geocode_location
     result = Geocoder.search(ip_address).first
     return unless result
-
+  
+    lat, lng = result.data["loc"]&.split(",")&.map(&:to_f)
+    return unless lat && lng
+  
     update_columns(
-      country: result.country,
-      region: result.state,
-      city: result.city,
-      latitude: result.latitude,
-      longitude: result.longitude
+      country: result.data["country"],
+      region: result.data["region"],
+      city: result.data["city"],
+      latitude: lat,
+      longitude: lng
     )
   end
 end
