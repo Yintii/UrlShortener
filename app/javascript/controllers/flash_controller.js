526af99ff996b2c2ad66c5127d47d2173ff39e51
yintii <keleheart1@gmail.com>
2026-02-25 20:26:29 -0800

    flash messages now fade away after a moment

--- app/javascript/controllers/flash_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  static targets = ["flash"]
-
-  connect() {
-    console.log("Flash controller connected")
-  }
-}   
