939b2b8612f3ef3ec39a230e9d0a5f874f44713d
yintii <keleheart1@gmail.com>
2026-02-25 19:21:29 -0800

    added gravatar images, gem was abandonded so I made a helper in /helpers/application_helper.rb for it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def gravatar_image_tag(email, options = {})
+    hash = Digest::MD5.hexdigest(email.to_s.downcase.strip)
+    size = options.delete(:size) || 80
+    alt  = options.delete(:alt) || email
+    secure = options.dig(:gravatar, :secure) != false
+
+    base = secure ? "https://secure.gravatar.com" : "http://www.gravatar.com"
+    url  = "#{base}/avatar/#{hash}?s=#{size}"
+
+    image_tag(url, alt: alt, **options.except(:gravatar))
+  end
 end
