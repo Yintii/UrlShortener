c1789810dbf5fc9572e3e7187a601f0323089b5c
yintii <keleheart1@gmail.com>
2026-02-27 20:42:00 +0000

    fixed issue by changing the download to be handled by the server instead

--- app/controllers/short_links_controller.rb
+++ app/controllers/short_links_controller.rb
@@ -29,9 +29,26 @@ class ShortLinksController < ApplicationController
   end
 
   def qr_download
-    send_data params[:svg_data],
-      filename: 'qr_code.svg',
-      type: 'image/svg+xml',
+    record = ShortLink.find_by(short_link: params[:short_code], link_type: "qr")
+    return redirect_to root_path if record.nil?
+  
+    qrcode = RQRCode::QRCode.new(record.original_link)
+    png = qrcode.as_png(
+      bit_depth: 1,
+      border_modules: 4,
+      color_mode: ChunkyPNG::COLOR_GRAYSCALE,
+      color: "black",
+      file: nil,
+      fill: "white",
+      module_px_size: 6,
+      resize_exactly_to: false,
+      resize_orth: false,
+      size: 300
+    )
+  
+    send_data png.to_blob,
+      filename: 'qr_code.png',
+      type: 'image/png',
       disposition: 'attachment'
   end
 
